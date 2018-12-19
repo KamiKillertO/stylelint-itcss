99dab0c6dbf4ca3c38131e24f8bc204480b445fa
benjaminJ <kamikillerto@gmail.com>
2018-12-19 15:07:55 +0100

    chore: install and setup ava for the testing

---
+++ ava.config.js
@@ -0,0 +1,18 @@
+export default {
+  "files": [
+    "dist/**/__tests__/*.js"
+  ],
+  // "sources": [
+  //   "dist/**/__tests__/*.js"
+  // ],
+  // "match": [
+  //   "dist/**/__tests__/*.js"
+  // ],
+  "cache": true,
+  "concurrency": 5,
+  "failFast": true,
+  "failWithoutAssertions": false,
+  "tap": true,
+  "verbose": true,
+  "compileEnhancements": true
+}
